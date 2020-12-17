25d7fc10d24aaab3e44e0923e922fb2b3e54ea69
jgindis <jgindis20@stuy.edu>
2020-12-17 14:19:10 -0500

    Added selectionSort()

--- Sorts.java
+++ Sorts.java
@@ -6,8 +6,8 @@ public class Sorts{
   public static void bubbleSort(int[] data) {
     for (int i = 0; i < data.size(); i++) {
       int cur = 0;
-      for(int i = 0;i < data.length;i++){
-        for(int j = i + 1;j < data.length;j++){
+      for(int i = 0; i < data.length; i++){
+        for(int j = i++; j < data.length; j++){
           if(data[i] > data[j]){
             cur = data[i];
             data[i] = data[j];
@@ -18,5 +18,8 @@ public class Sorts{
     }
   }
 
+  public static void selectionSort(int[] data){
+    
+  }
 
 }
