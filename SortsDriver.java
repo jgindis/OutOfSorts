9f646442be71e41d4be51e4ad96d00639f714575
jgindis <jgindis20@stuy.edu>
2020-12-21 13:11:17 -0500

    Added generateArr() to generate arrays with random elements

--- SortsDriver.java
+++ SortsDriver.java
@@ -5,5 +5,13 @@ public class SortsDriver{
 
   }
 
+  public static int[] generateArr(int length, int seed) {
+    int[] array = new int[length];
+    for (int i = 0; i < length; i++) {
+      Random rng = new Random();
+      array[i] = rng.nextInt();
+    }
+    return array;
+  }
 
 }
